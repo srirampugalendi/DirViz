b2add611aa32d79d33cab03d3549d5f529305c86
srirampugalendi <shriramp109@gmail.com>
2019-09-24 23:03:56 +0800

    Initial comit

---
+++ TestApp1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TestApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resetButton = new System.Windows.Forms.Button();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.expandOrCollapseButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.matchCase = new System.Windows.Forms.CheckBox();
+            this.domainNameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetButton.Location = new System.Drawing.Point(905, 9);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(136, 23);
+            this.resetButton.TabIndex = 0;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // treeView
+            // 
+            this.treeView.AllowDrop = true;
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.Location = new System.Drawing.Point(12, 38);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(1029, 472);
+            this.treeView.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(735, 9);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchBox
+            // 
+            this.searchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBox.Location = new System.Drawing.Point(508, 10);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(221, 20);
+            this.searchBox.TabIndex = 3;
+            this.searchBox.Text = "Enter Search Text";
+            this.searchBox.Click += new System.EventHandler(this.SearchBox_Click);
+            // 
+            // expandOrCollapseButton
+            // 
+            this.expandOrCollapseButton.Location = new System.Drawing.Point(390, 8);
+            this.expandOrCollapseButton.Name = "expandOrCollapseButton";
+            this.expandOrCollapseButton.Size = new System.Drawing.Size(112, 23);
+            this.expandOrCollapseButton.TabIndex = 4;
+            this.expandOrCollapseButton.Text = "Expand/Collapse All";
+            this.expandOrCollapseButton.UseVisualStyleBackColor = true;
+            this.expandOrCollapseButton.Click += new System.EventHandler(this.ExpandOrCollapseButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 9);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 5;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // matchCase
+            // 
+            this.matchCase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.matchCase.AutoSize = true;
+            this.matchCase.Location = new System.Drawing.Point(816, 13);
+            this.matchCase.Name = "matchCase";
+            this.matchCase.Size = new System.Drawing.Size(83, 17);
+            this.matchCase.TabIndex = 6;
+            this.matchCase.Text = "Match Case";
+            this.matchCase.UseVisualStyleBackColor = true;
+            // 
+            // domainNameBox
+            // 
+            this.domainNameBox.Location = new System.Drawing.Point(93, 9);
+            this.domainNameBox.Name = "domainNameBox";
+            this.domainNameBox.Size = new System.Drawing.Size(291, 20);
+            this.domainNameBox.TabIndex = 7;
+            this.domainNameBox.Text = "Enter Domin Name <http://example.com/>";
+            this.domainNameBox.Click += new System.EventHandler(this.DomainNameBox_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1053, 522);
+            this.Controls.Add(this.domainNameBox);
+            this.Controls.Add(this.matchCase);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.expandOrCollapseButton);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.resetButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MinimumSize = new System.Drawing.Size(1069, 561);
+            this.Name = "Form1";
+            this.Text = "DirectoryVizualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Button expandOrCollapseButton;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.CheckBox matchCase;
+        private System.Windows.Forms.TextBox domainNameBox;
+    }
+}
+
---
+++ TestApp1/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace TestApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        bool isExpanded = true;
+
+        public Form1()
+        {   
+            InitializeComponent();
+  
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchBox_Click(object sender, EventArgs e)
+        {
+            searchBox.Clear();
+        }
+
+        private void ExpandOrCollapseButton_Click(object sender, EventArgs e)
+        {
+            
+
+            if (isExpanded)
+            {
+                treeView.CollapseAll();
+                isExpanded = false;
+            }
+            else
+            {
+                treeView.ExpandAll();
+                isExpanded = true;
+            }
+            
+        }
+
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            this.treeView.Nodes.Clear();
+            this.treeView.Invalidate();
+            
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            int result = -1;
+            string fileName = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                result = checkType(openFileDialog1.FileName);
+                fileName = openFileDialog1.FileName;
+
+            }
+
+            //string fileName = filename1;
+            if (result == 1)
+            {
+                FILEITEM.ParseFileWindows(fileName, this);
+            }
+            if (result == 2)
+            {
+                StreamReader reading = File.OpenText(fileName);
+                string str = reading.ReadLine();
+                string text = File.ReadAllText(fileName);
+                text = text.Replace("./", str+"/");
+                fileName = fileName + "_tmp";
+                File.WriteAllText(fileName, text);
+                FILEITEM.ParseFileLinux(fileName, this);
+            }
+            this.treeView.Nodes.Clear();
+            this.treeView.Invalidate();
+            
+
+            FILEITEM.MakeTree(FILEITEM.files, null, treeView, 0);
+
+            treeView.ExpandAll();
+            isExpanded = true;
+        }
+
+        private void DomainNameBox_Click(object sender, EventArgs e)
+        {
+            domainNameBox.Clear();
+        }
+
+        private int checkType(String textFile)
+        {
+            //Console.WriteLine(fileName); 123
+            // Read file using StreamReader. Reads file line by line
+            //Console.WriteLine(textFile);
+            Console.WriteLine(textFile);
+            int result = 0;                 // 1 - Win, 2 - Unix, 3 - Invalid file
+            using (StreamReader file = new StreamReader(textFile))
+                {
+                    Console.WriteLine(textFile);
+                    
+                    string ln;
+
+                    ln = file.ReadLine();
+                    //Console.WriteLine(ln);
+                    if (ln.Contains("Volume in drive"))
+                    {
+                    Console.WriteLine("Valid Windows directory structure detected ");
+                    result = 1;
+                    }
+                    else if(ln.StartsWith("/") || ln.Contains(".:"))
+                    {
+                    Console.WriteLine("Valid Linux directory structure detected ");
+                    result = 2;
+                    }
+                    else
+                    {
+                    Console.WriteLine("Invalid Directory Listing File");
+                    result = 3;
+                    }
+                    
+                }
+         return result;
+        }
+
+    }
+
+    public class FILEITEM
+    {
+        const string DIRECTORY_OF = "Directory of";
+        const string REGEX_DATE_PATTERN = @"\d{2}/\d{2}/\d{4}\s\s\d{2}:\d{2}\s[AP]M";
+        const string REGEX_FILENAME_PATTERN = @"[^\s]+";
+
+        public static List<FILEITEM> files = new List<FILEITEM>();
+        public string[] folder { get; set; }
+        public string filename { get; set; }
+        public DateTime time { get; set; }
+
+        public static void ParseFileWindows(string filename, Form1 form1)
+        {
+            CultureInfo info = CultureInfo.InvariantCulture;
+            StreamReader reader = new StreamReader(filename);
+            files.Clear();
+            string[] directory = null;
+
+            string line = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Contains(DIRECTORY_OF))
+                {
+                    directory = line.Substring(line.IndexOf(DIRECTORY_OF) + DIRECTORY_OF.Length).Trim().Split(new char[] { '\\' }).ToArray();
+                    Console.WriteLine(line);
+                    Array.ForEach(directory, Console.WriteLine);
+                }
+                else
+                {
+                    Match dateMatch = Regex.Match(line, REGEX_DATE_PATTERN);
+                    if (dateMatch.Success && !line.Contains("<DIR>"))
+                    {
+                        Match filenameMatch = Regex.Match(line.Trim(), REGEX_FILENAME_PATTERN, RegexOptions.RightToLeft);
+                        FILEITEM newFileItem = new FILEITEM() { folder = directory, filename = filenameMatch.Value };
+                        files.Add(newFileItem);
+                    }
+                }
+            }
+        }
+
+        public static void ParseFileLinux(string filename, Form1 form1)
+        {
+
+            const string DIRECTORY_OF = "/";
+            const string REGEX_DATE_PATTERN = @"[d][l,r,w,x,t,-]{9}\s";
+            const string REGEX_IS_FILE = @"[-][l,r,w,x,t,-]{9}\s";
+            const string REGEX_FILENAME_PATTERN = @"[^\s]+";
+
+            CultureInfo info = CultureInfo.InvariantCulture;
+            StreamReader reader = new StreamReader(filename);
+            files.Clear();
+            string[] directory = null;
+
+            string line = "";
+            while ((line = reader.ReadLine()) != null)
+            {
+                //Console.WriteLine(line);
+                if (line.StartsWith(DIRECTORY_OF))
+                {   
+                    if (line.EndsWith(":"))
+                    {
+                        line = line.Substring(0, line.Length - 1);
+                    }           
+                    directory = line.Substring(line.IndexOf(DIRECTORY_OF) + DIRECTORY_OF.Length).Trim().Split(new char[] { '/' }).ToArray();
+                    Console.WriteLine(line);
+                    //Console.WriteLine(line.Substring(line.IndexOf(DIRECTORY_OF) + DIRECTORY_OF.Length).Trim().Split('/'));
+                    Array.ForEach(directory, Console.WriteLine);
+                }
+                else
+                {
+                    Match dateMatch = Regex.Match(line, REGEX_IS_FILE);
+                    if (dateMatch.Success)
+                    {
+                        //Console.WriteLine(line);
+                        Match filenameMatch = Regex.Match(line.Trim(), REGEX_FILENAME_PATTERN, RegexOptions.RightToLeft);
+                        //string fileNameStripped = filenameMatch.Value.Substring(6);
+                        FILEITEM newFileItem = new FILEITEM() { folder = directory, filename = filenameMatch.Value };
+                        files.Add(newFileItem);
+                    }
+                }
+            }
+        }
+        public static void MakeTree(List<FILEITEM> files, TreeNode node, TreeView treeview1, int index)
+        {
+            TreeNode childNode = null;
+            var groups = files.Where(x => x.folder.Length > index).GroupBy(x => x.folder[index]).ToList();
+            foreach (var group in groups)
+            {
+                childNode = new TreeNode(group.Key);
+                if (node == null)
+                {
+                    treeview1.Nodes.Add(childNode);
+                }
+                else
+                {
+                    node.Nodes.Add(childNode);
+                }
+                foreach (FILEITEM item in group)
+                {
+                    if (item.folder.Length - 1 == index)
+                    {
+                        TreeNode fileNode = new TreeNode(string.Join(",", new string[] { item.filename }));
+                        childNode.Nodes.Add(fileNode);
+                    }
+                }
+                MakeTree(group.ToList(), childNode, null, index + 1);
+            }
+        }
+    }
+
+}
